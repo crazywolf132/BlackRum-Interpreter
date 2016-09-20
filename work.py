14598581b2e665ac41bcf57723f9283b610a925e
crazywolf132 <crazywolf132@gmail.com>
2016-09-20 20:03:36 +1000

    My files.

---
+++ work.py
@@ -0,0 +1,28 @@
+import blackrum
+import subprocess
+
+sourcecode = """
+npp ppp ppp pp
+onp ppp ppp ppp pnp ppp ppp ppp pnp ppp ppp ppp pnp pp pp
+ppp pppn ppp ppp ppp ppp npp ppn ppp ppp pnp ppp ppp ppp npp ppp ppp
+ppp npp ppp pp
+ppp pnp ppp ppp ppp pnp ppp npp ppp ppp ppp pnp ppp ppp ppp pnp ppp ppp
+ppp pnp ppp ppp ppp pnp ppp ppp ppp npp pnp ppp nbb bbb bbb bbb bbb bbb
+bbb mc
+npp npp ppn mmm mmn nmm mmn mnpp npnm mnm mnp npp ppn mnp npp ppn mmn np
+ppn mn
+bbb bbb bbb bbb bbb bbb bb
+nrn rnr nrn rnr nrn rnr nrn rn rnrn rnr nrnr nrnr
+"""
+
+command = 'blackrum.evaluate(sourcecode)'
+exec(command)
+
+#out = subprocess.call(["python", "blackrum.py", "run.br"])
+#out
+#p = subprocess.Popen(['python', 'blackrum.py', 'work.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#out, err = p.communicate()
+#code = out
+#print out
+#exec code
+#exec out
