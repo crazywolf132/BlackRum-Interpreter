481aa37c61e22255679d777a94dad993635d2584
crazywolf132 <crazywolf132@gmail.com>
2016-09-26 19:17:52 +1000

    Update and rename work.py to run.py

---
+++ run.py
@@ -0,0 +1,25 @@
+import blackrum
+import sys
+
+sourcecode = """
+npp ppp ppp pp
+onp ppp ppp ppp pnp ppp ppp ppp pnp ppp ppp ppp pnp pp pp
+ppp pppn ppp ppp ppp ppp npp ppn ppp ppp pnp ppp ppp ppp npp ppp ppp
+ppp npp ppp pp
+ppp pnp ppp ppp ppp pnp ppp npp ppp ppp ppp pnp ppp ppp ppp pnp ppp ppp
+ppp pnp ppp ppp ppp pnp ppp ppp ppp npp pnp ppp nbb bbb bbb bbb bbb bbb
+bbb mc
+npp npp ppn mmm mmn nmm mmn mnpp npnm mnm mnp npp ppn mnp npp ppn mmn np
+ppn mn
+bbb bbb bbb bbb bbb bbb bb
+nrn rnr nrn rnr nrn rnr nrn rn rnrn rnr nrnr nrnr
+"""
+
+#This ^^^ is my own coding language. It is print'hello,world!' (python code) translated.
+
+
+command = testing.BlackRum(sourcecode)
+# This ^^^ command converts the above sourcecode into english.
+print(command)
+# Seen as the command variable will output print'hello,world!', i would like to run that as a command.
+# So, if you understand what i mean. It should output hello,world! in the console.
